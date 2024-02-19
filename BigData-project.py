95a2a62aee9dc38ac0e1b3a8ac330856e8a790a5
trumabrat <trumabrat>
2024-02-19 21:30:49 +0000

    big progress, needs some comments to finish

--- BigData-project.py
+++ BigData-project.py
@@ -82,21 +82,30 @@
 # MAGIC
 # MAGIC ![second iteration ccf](https://i.ibb.co/ky5vFBd/ccf-second-iteration.png)
 # MAGIC
-# MAGIC We can determine what the algorithm does: it groups, for every key vertex, associated vertices, such that there exists a path between the vertices in the values iterable and the key vertix. This means that there is also a path between any two vertix contained in values, and that all nodes in the values iterable + the key node are necessarily in the same component. The next step is then to map all the values + key to the smallest node id in the set (except if the key is the smallest node). We do this because we have set the identifier of the component to be the smallest value. By doing this in an iterative setting, thanks to this logic and deduplication, we are bound to arrive to the desired state described above.
+# MAGIC We can determine what the algorithm does: it groups, for every key vertex, associated vertices, such that there exists a path between the vertices in the values iterable and the key vertix. This means that there is also a path between any two vertix contained in the values iterable, and that all nodes in the values iterable + the key node are necessarily in the same component. The next step is then to map all the values + key to the smallest node id in the set (except if the key is the smallest node). We do this because we have set the identifier of the component to be the smallest value. By doing this in an iterative manner, thanks to this logic and deduplication, we are bound to arrive to the desired state described above.
 # MAGIC
-# MAGIC <h1 style="text-color:red;"> A Améliorer </h1>
+# MAGIC The second, improved version of CCF Iterate proposed by the authors, takes advantage of Hadoop's shuffle and sort phase to pass, to the reducer function, an ordered list of values instead of an unordered list. The advantage is that, when the \<values\> iterable is sorted, instead of traversing the whole iterable at the beginning of the algorithm in order to find the minimum, we know that the minimum value is located at the index 0 of the iterable. While this optimization might seem insignificant, as the size of the graph, and more particularly as the sizes of connected components continue to increase, this becomes very impactful for the algorithm's performance.
+# MAGIC
+# MAGIC ![CCF algorithm with sorted values](https://i.ibb.co/vvZ53DW/ccf-iterate-secondary-sorting.png)
+# MAGIC
+# MAGIC In order for this algorithm to work, however, the \<values\> iterable needs to, of course, be sorted. By default, Hadoop's Shuffle & Sort phase is performed on the **keys** and not the values. Furthermore, contrary to Google's implementation of MapReduce, Hadoop doesn't support secondary sorting. In order to provide values as a sorted array to the reduce function, two things need to happen:
+# MAGIC  1. We need to create a composite key, that will contain both the key and the value pair, for each pair that was emitted during the map phase. This means that we will transform, for example, the (key,value) output of the map phase into ((key,value), _ ), where (key,value) form together the composite key that will be used during the shuffle and sort phase. By default, when Hadoop's shuffle phase takes as input a composite key, it will sort by the first element of the key, than the second, than the third, and so on... This means that, at the end of our Shuffle and Sort phase, the \<values\> iterable will be in correct order. At that point, we need to remove the composite key before executing the reduce phase.
+# MAGIC  2. While we want to have a composite key to benefit from the "sorting" functionality of the shuffle and sort phase, we still need to shuffle the data according to the real key and not the intermediate, composite key. In order to do this, we also need to implement a custom partitioner that will apply the hash based only on the key value of our composite key, instead of applying the hash to the entire tuple (which would have been the default behavior). 
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC ## Implementation
 # MAGIC
-# MAGIC In this section, we will implement the proposed solution in Spark. We will implement two classes, one without second-order sorting and the other with second-order sorting, and see the difference in performance
+# MAGIC In this section, we will implement the proposed solution in Spark
+# MAGIC  - **NaiveCCF** is the class that implements the unoptimized version of CCF described in the paper, without second-order sorting.
+# MAGIC  - **SortingCCF** computes the intermediate, sorting step, that allows us not to iterate over the \<values\> iterable in order to find a minimum. We implemented the sorting step a little differently than the method we described for Hadoop. In fact, we first tried to implement the "Hadoop strategy" by producing composite keys, repartitioning (and sorting, since by default, Spark only does a Shuffle, and not a Shuffle & Sort) with a custom partinioner and then eliminating the composite key (you can see the code in the Annexe), however, during our benchmarking, we found that this method was less efficient compared to NaiveCCF. The currently implemented method utilizes python dictionaries and sets to group all values with their respective key. During the sorting phase, this method is more memory intensive compared to the "Hadoop method", it is a trade-off between speed and memory.
 
 # COMMAND ----------
 
 """
 Here, we define the example graph that was used to explain CCF
+We will use this simple example for our first tests of our implementations 
 """
 
 NUM_PARTITIONS = 4
@@ -109,22 +118,23 @@ book_example = sc.parallelize([
     (7, 8)
 ], NUM_PARTITIONS)
 # After approach, we expect [(8, 6), (5, 1), (4, 1), (3, 1), (2, 1), (7, 6)], newCouples = [4,9,4,0]
-# Number of components is, output of connected_components should be: [(1, [5, 4, 3, 2]), (6, [8, 7])]
+# Number of components is 2, output of the connected_components function should be: [(1, [5, 4, 3, 2]), (6, [8, 7])]
 
 # COMMAND ----------
 
 from pyspark import RDD
 
-'''
-Defining accumulator
-'''
-def connected_components(rdd: RDD):
+def connected_components(rdd: RDD) -> RDD:
         """
-        Global function used to find the number of connected components and all the components once the CCF has converged
+        This function takes in the resulting rdd after the CCF algorithm has converged. It reverses the key and value pairs of the rdd and then returns the rdd grouped by keys.
+        As a result, the returned rdd is the collection of (componentID, list [componentNodes]), which together make up the whole connected component. From here, we can easily get the
+        number of connected components in the graph by running the action .count(), or we can print all nodes in the connected component, count the number of nodes per component, ...
+        
         Arguments:
-            rdd: PythonRDD after cff_run transformation
+            rdd: RDD after cff_run transformation
             
-        Returns: All connected components, as a list of (componentID, componentNodes (iterable))
+        Returns:
+            new_rdd: RDDAll connected components, as a list of (componentID, componentNodes (iterable))
         """
         new_rdd = rdd.map(lambda x: (x[1],x[0])).groupByKey()
         return new_rdd
@@ -199,17 +209,11 @@ class NaiveCCF:
             newPairsByIteration.append(pairCount.value)
         
         return (new_rdd, newPairsByIteration)
-    
+
+# The following code can be used to test our class on a simple graph, the one we used to explain CCF.  
 naive_executor = NaiveCCF(rdd=book_example)
 connected_components(naive_executor.ccf_run()[0]).mapValues(list).collect()
 
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ### Optimizations
-# MAGIC
-# MAGIC This first implementation can be optimized by adding second-order sorting. In fact, in the reduce function of cff_iterate, we need to traverse the whole array of associated values to find the minimum. If this array was sorted in ascending order during the groupBy transformation, it would be sufficient to just take the first element of the array.
 
 # COMMAND ----------
 
@@ -294,8 +298,9 @@ class SortingCCF:
         return (new_rdd, newPairsByIteration)
 
 
-executor = SortingCCF(rdd=book_example)
-executor.ccf_run()[0].collect()
+# Again, this code is used to test our class against the simple graph example
+sorting_executor = SortingCCF(rdd=book_example)
+connected_components(sorting_executor.ccf_run()[0]).mapValues(list).collect()
 
 # COMMAND ----------
 
@@ -305,8 +310,9 @@ executor.ccf_run()[0].collect()
 # MAGIC In this section, we will test the execution of both algorithms against the same graph that was used in the connected compontent finder paper. We will first start by importing our dataset that can be found on here:
 # MAGIC [Link](https://snap.stanford.edu/data/web-Google.html)
 # MAGIC
-# MAGIC This graph is represent only by its set of edges. It is a directed graph with 5 105 039 edges, and 875 713nodes
+# MAGIC This graph is represented only by its set of edges. It is a directed graph with 5 105 039 edges, and 875 713 nodes.
 # MAGIC
+# MAGIC We will start by converting to graph from its raw form (in our case, we saved the data in a table, make sure to adapt this portion of the code) to a collection of (key, value) pairs. However, this alone isn't enough. This graph might be a directed graph, even if finding connected components is fundamentally a undirected graph problem. We will implement logic to test if the graph is a directed or undirected graph, and if the former is the case we will transform the graph to be an undirected graph
 
 # COMMAND ----------
 
@@ -326,7 +332,8 @@ def determine_graph_nature(input_rdd):
     """
     Arguments:
         input_rdd: the initial rdd
-    Output:
+
+    Retuns:
         inverse_rdd_initial_size: the initial size of the inverse rdd = the initial size of the rdd
         inverse_rdd_sub_size: Size of the rdd resulting from the subtraction of the input_rdd from the inverse_rdd
     """
@@ -370,13 +377,22 @@ web_undirected_graph = rdd.map(undirected_graph_map).distinct().persist()
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC We now have our undirected graph, that we will use for our experimentations
+# MAGIC We now have our undirected graph, that we will use for our experimentations. Let's define a function to measure the execution time of both approaches.
 
 # COMMAND ----------
 
 import time
 
-def measure_execution_time(executor):
+def measure_execution_time(executor) -> None:
+    """
+    This functions takes as input either a NaiveCCF or a SortingCCF object, and runs the ccf_run operation.
+    It doesn't return anything, the function just prints out the result of the operation and the execution time
+    
+    Arguments:
+        executor: NaiveCCF or SortingCCF object
+    Returns:
+        None
+    """
     start_time = time.time_ns()
     result = executor.ccf_run()
     end_time = time.time_ns()
@@ -416,6 +432,22 @@ measure_execution_time(sorted_executor)
 # Number of iterations: 8
 # New couples per iteration: [7223780, 4758451, 3278772, 3888454, 1905323, 86783, 1318, 0]
 # Number of connected components: 2746
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC We obtained these results by running the code on a single node with 14 GB of Unified memory and 4 cores (which is why we made the choice for 4 partitions)
+# MAGIC
+# MAGIC We can see that we get the same output from both functions:
+# MAGIC  - **Number of iterations**: 8
+# MAGIC  - **Number of connected components**:  2746
+# MAGIC
+# MAGIC  As for the exeuction time:
+# MAGIC |                              | NaiveCCF | SortingCCF |
+# MAGIC |--------------------|---------|----------|
+# MAGIC | **Execution time (seconds)** | 99        | 89          |
+# MAGIC
+# MAGIC Our efficient implementation of CCF was faster by 10 seconds compared to the naive implementation, which means that there was a 12.5% improvement. However, we have to acknowledge that our testing conditions aren't perfect, since the code is a run on a single machine, and thus, doesn't actually need to send data over the network when executing a shuffle. However, in theory, this shouldn't affect the fact that SortingCCF is more efficient since, with each shuffle, both implementations should have the exact same number of data (key,value) pairs that are being shuffled.  
 
 # COMMAND ----------
 
@@ -576,13 +608,16 @@ SortingCFF.connected_components(SortingCFF.cff_run(book_example)[0]).mapValues(l
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ### Sorting CFF second try
+# MAGIC ### Sorting CFF second try - Hadoop like secondary sort
 
 # COMMAND ----------
 
 # experimenting with repartitionAndSortWithinPartitions
 from pyspark.rdd import portable_hash
 class SortingCFF2:
+    """
+    This implementation is similar to how we would implement secondary sorting in hadoop
+    """
     def cff_iterate(rdd: RDD) -> RDD:
         """
         Arguments:
@@ -616,15 +651,6 @@ class SortingCFF2:
             # Now we yield all results for each key in the partition
             for key, values in grouped_key_values.items():
                 yield (key,values)
-        
-        def group_by_partitions_unsorted(rdd_partition):
-            grouped_key_values = dict()
-            for key,value in rdd_partition:
-                if key not in grouped_key_values:
-                    grouped_key_values[key] = set()
-                grouped_key_values[key].add(value)
-            for key, values in grouped_key_values.items():
-                yield (key,sorted(values))
 
         def cff_reduce(x):
             res = []
@@ -641,30 +667,13 @@ class SortingCFF2:
                     newPairCounter.add(1)
             return res
         
-        # return (rdd.flatMap(cff_map)
-        #         .keyBy(lambda kv: (kv[0],kv[1])) # We create a temporary composite key
-        #         .repartitionAndSortWithinPartitions(numPartitions = NUM_PARTITIONS,partitionFunc= partitioner(NUM_PARTITIONS),ascending=True,keyfunc=lambda x:(x[0],x[1])) # we sort by key and then by value
-        #         .map(lambda x: x[1]) # We remove the composite key
-        #         .groupByKey()
-        #         .flatMap(cff_reduce)
-        #         .distinct(), newPairCounter)
-        # return (rdd.flatMap(cff_map)
-        #         .keyBy(lambda kv: (kv[0],kv[1])) # We create a temporary composite key
-        #         .repartitionAndSortWithinPartitions(numPartitions = NUM_PARTITIONS,partitionFunc= partitioner(NUM_PARTITIONS),ascending=True,keyfunc=lambda x:(x[0],x[1])) # we sort by key and then by value
-        #         .map(lambda x: x[1]) # We remove the composite key
-        #         .mapPartitions(group_by_partitions)
-        #         .flatMap(cff_reduce)
-        #         .distinct(), newPairCounter)
-        # return (rdd.flatMap(cff_map)
-        #         .partitionBy(NUM_PARTITIONS)
-        #         .mapPartitions(group_by_partitions_unsorted)
-        #         .flatMap(cff_reduce)
-        #         .distinct(), newPairCounter)
         return (rdd.flatMap(cff_map)
-                .partitionBy(NUM_PARTITIONS)
-                .mapPartitions(group_by_partitions_unsorted)
-                .flatMap(cff_reduce), newPairCounter)
-                
+                .keyBy(lambda kv: (kv[0],kv[1])) # We create a temporary composite key
+                .repartitionAndSortWithinPartitions(numPartitions = NUM_PARTITIONS,partitionFunc= partitioner(NUM_PARTITIONS),ascending=True,keyfunc=lambda x:(x[0],x[1])) # we sort by key and then by value
+                .map(lambda x: x[1]) # We remove the composite key
+                .groupByKey()
+                .flatMap(cff_reduce)
+                .distinct(), newPairCounter)
     
     def cff_run(rdd: RDD, iterate=cff_iterate):
             """
